4b5a7bbdae4c147a3f7b5d3b3f31191eaea8d97d
arunkumark <137906978+Arunkumarkdeveloper@users.noreply.github.com>
2024-02-23 15:35:07 +0530

    changes updated

---
+++ src/app/robots.tsx
@@ -0,0 +1,13 @@
+"use server";
+import { MetadataRoute } from "next";
+
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private",
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  };
+}
