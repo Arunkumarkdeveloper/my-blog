15c05aeb4f074ba4a177349839ff9d5ae5bc5372
arunkumark <137906978+Arunkumarkdeveloper@users.noreply.github.com>
2024-02-29 19:10:48 +0530

    api updated

--- src/app/api/like/route.tsx
+++ src/app/api/like/route.tsx
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ConnectDB } from "@/backend/ConnectDB";
 import LikeSchema from "@/backend/LikeSchema";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const GET = async (requst: NextRequest) => {
+  noStore();
   await ConnectDB();
   const likes = await LikeSchema.find();
   return NextResponse.json(likes, { status: 200 });
