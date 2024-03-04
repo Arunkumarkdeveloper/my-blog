4d6017aa9adcadb609a9ebf79a70fedf4b6cd6f7
arunkumark <137906978+Arunkumarkdeveloper@users.noreply.github.com>
2024-03-04 16:03:40 +0530

    fixes updated

--- src/app/api/saved-post/route.tsx
+++ src/app/api/saved-post/route.tsx
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ConnectDB } from "@/backend/ConnectDB";
 import SavedPosts from "@/backend/SavedPosts";
+import { unstable_noStore as noStore } from "next/cache";
 
 export const GET = async (request: NextRequest) => {
+  noStore();
   await ConnectDB();
   const saved_posts = await SavedPosts.find();
   return NextResponse.json(saved_posts);
